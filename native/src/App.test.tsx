2e02043f8f4197ce41fd3bee3165eaad8e3cf18a
Mia Pelino <miapelino1@gmail.com>
2020-08-12 15:28:44 -0700

    add high level testing for post fetch and display

--- native/src/App.test.tsx
+++ native/src/App.test.tsx
@@ -1,11 +1,77 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import renderer, { act, create } from 'react-test-renderer';
+import { mocked } from 'ts-jest/utils';
+import 'isomorphic-fetch';
+import useGetLatinPostService from './services/useGetLatinPostService';
+import { Service } from './types/Service';
+import { LatinPost } from './types/LatinPost';
 import { App } from './App';
+
+jest.mock('./services/useGetLatinPostService');
+const mockedService = mocked(useGetLatinPostService, true);
 
 describe('<App />', () => {
   test('renders without crashing', () => {
+    let tree;
+
+    const expectedResult: Service<LatinPost> = {
+      status: 'loading',
+    };
+    mockedService.mockReturnValueOnce(expectedResult);
+
+    act(() => {
+      tree = renderer.create(<App />).toJSON();
+    });
+
+    expect(tree).toBeNull();
+  });
+
+  it('renders ActivityIndicator component while loading', () => {
+    const expectedResult: Service<LatinPost> = {
+      status: 'loading',
+    };
+    mockedService.mockReturnValueOnce(expectedResult);
+
     const tree = renderer.create(<App />).toJSON();
-    expect(tree).not.toBeNull();
+
+    expect(tree.children[0].type).toBe('ActivityIndicator');
+    expect(tree.children[0].type).not.toBe('Text');
+  });
+
+  it('displays number of posts returned when loaded', () => {
+    const expectedResult: Service<LatinPost> = {
+      status: 'loaded',
+      payload: [{}, {}, {}],
+    };
+    mockedService.mockReturnValueOnce(expectedResult);
+
+    const tree = renderer.create(<App />).toJSON().children[0];
+    console.log(tree);
+
+    expect(tree.type).not.toBe('ActivityIndicator');
+    expect(tree.children[0] + tree.children[1]).toBe('3 posts have loaded!');
+  });
+
+  it('displays error message when loaded with error', () => {
+    const expectedResult: Service<LatinPost> = {
+      status: 'error',
+      error: new Error(),
+    };
+    mockedService.mockReturnValueOnce(expectedResult);
+
+    const tree = renderer.create(<App />).toJSON().children[0];
+
+    expect(tree.type).not.toBe('ActivityIndicator');
+    expect(tree.children[0]).toBe('Oops, I did it again.');
+  });
+
+  it('matches the snapshot', () => {
+    const expectedResult: Service<LatinPost> = {
+      status: 'loading',
+    };
+    mockedService.mockReturnValueOnce(expectedResult);
+
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
--- native/src/services/useGetLatinPostService.tsx
+++ native/src/services/useGetLatinPostService.tsx
@@ -12,7 +12,7 @@ const useGetLatinPostService = () => {
   });
 
   useEffect(() => {
-    fetch('http://10.0.0.21:4000/posts')
+    fetch('http://10.0.0.122:4000/posts')
       .then((response) => response.json())
       .then((response) => setResult({ status: 'loaded', payload: response }))
       .catch((error) => setResult({ status: 'error', error }));
