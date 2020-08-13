6387f43acfa0f634672b06cddda9883485fe179b
Mia Pelino <miapelino1@gmail.com>
2020-08-13 11:03:57 -0700

    sort post lists in reverse chronological order

--- native/src/components/PostList.tsx
+++ native/src/components/PostList.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { FlatList, View, Text, StyleSheet } from 'react-native';
 import { LatinPosts } from '../services/useGetLatinPostService';
+import { LatinPost } from '../types/LatinPost';
 
 const PostList: React.FC<any> = (postList: LatinPosts) => {
-  const formatSummary = (body: string) => body.split(/\n\n/)[1].slice(2, 40);
+  const sortPostsReverseChronologically = (posts: LatinPost[]): LatinPost[] => {
+    return [].slice
+      .call(posts)
+      .sort(
+        (newer: LatinPost, older: LatinPost): number =>
+          new Date(older.publishedAt).getTime() -
+          new Date(newer.publishedAt).getTime()
+      );
+  };
+
+  const sortedPosts = sortPostsReverseChronologically(postList.posts);
+
+  const formatSummary = (body: string): string =>
+    body.split(/\n\n/)[1].slice(2, 40);
+
+  const formatDate = (dateString: string): string => {
+    let options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString([], options);
+  };
 
   return (
     <FlatList
-      data={postList.posts}
+      style={styles.list}
+      data={sortedPosts}
       renderItem={({ item }) => (
-        <View style={styles.list}>
+        <View style={styles.posts}>
           <Text>Title: {item.title}</Text>
           <Text>Summary: {formatSummary(item.body)}</Text>
           <Text>Author: {item.author.name}</Text>
-          <Text>Date: {item.publishedAt}</Text>
+          <Text>Date: {formatDate(item.publishedAt.toString())}</Text>
         </View>
       )}
     />
@@ -24,6 +44,9 @@ export default PostList;
 
 const styles = StyleSheet.create({
   list: {
-    margin: 10,
+    width: '100%',
+  },
+  posts: {
+    margin: 15,
   },
 });
