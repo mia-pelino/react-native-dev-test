b144f2bb20f11fbccd3c3199bb426c94cb02c659
Mia Pelino <miapelino1@gmail.com>
2020-08-13 21:34:17 -0700

    add markdown formatter and styling to full post view

--- native/src/components/FullPost.tsx
+++ native/src/components/FullPost.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import { RootStackParamList } from '../types/RootStackParamList';
+import Markdown from 'react-native-markdown-display';
 
 type FullPostRouteProp = RouteProp<RootStackParamList, 'FullPost'>;
 
@@ -18,7 +25,7 @@ type Props = {
 
 const FullPost: React.FC<Props> = ({ route, navigation }) => {
   return (
-    <View style={styles.fullPost}>
+    <ScrollView style={styles.fullPost}>
       <TouchableOpacity
         onPress={() => {
           navigation?.navigate('Home');
@@ -28,9 +35,10 @@ const FullPost: React.FC<Props> = ({ route, navigation }) => {
       </TouchableOpacity>
       <View>
         <Text style={styles.title}>{route.params.title}</Text>
-        <Text style={styles.body}>{route.params.body}</Text>
+        {/* <Text style={styles.body}>{route.params.body}</Text> */}
+        <Markdown style={markdownStyle}>{route.params.body}</Markdown>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -42,19 +50,38 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   title: {
-    color: '#FFF',
-    paddingBottom: 25,
-    paddingTop: 25,
+    color: '#A5FFD6',
+    paddingTop: 20,
+    paddingBottom: 15,
     fontWeight: 'bold',
     fontSize: 24,
     alignSelf: 'center',
   },
   body: {
+    flex: 1,
+    flexWrap: 'wrap',
     color: '#FFF',
-    fontSize: 18,
+    fontSize: 16,
+    opacity: 1,
+    paddingHorizontal: 10,
   },
   navigateBack: {
     paddingTop: 10,
-    color: '#FFF',
+    color: '#FFA69E',
   },
 });
+
+const markdownStyle = {
+  body: {
+    ...styles.body,
+    fontSize: 14,
+  },
+  heading1: {
+    ...styles.body,
+    fontSize: 22,
+  },
+  heading2: {
+    ...styles.body,
+    fontSize: 20,
+  },
+};
